1eb1b1104f16bef8b814847d4a9ec40d864e457b
AdhamA7med <157298192+AdhamA7med@users.noreply.github.com>
2025-06-10 11:02:10 +0300

    Update WhatsAppFloat.tsx

--- src/components/WhatsAppFloat.tsx
+++ src/components/WhatsAppFloat.tsx
@@ -4,7 +4,7 @@ import { MessageSquare } from 'lucide-react';
 
 const WhatsAppFloat = () => {
   const openWhatsApp = () => {
-    window.open('https://wa.me/201018485283', '_blank');
+    window.open('https://wa.me/201500841180', '_blank');
   };
 
   return (
